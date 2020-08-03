73a4ac11ec423656a0a8013c3b615dc17a444919
Alena <kondruh_alena@mail.ru>
2020-08-03 17:01:06 +0300

    template

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Главная</router-link> |
+      <router-link to="/about">Рецепты</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+  #app {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    color: #222;
+    text-align: center;
+  }
 
 #nav {
   padding: 30px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <add-recipe-component/>
+    <hr>
+    <div class="columns">
+      <recipe-list-component/>
+      <recipe-detail-component/>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AddRecipeComponent from '../components/add-recipe-component'
+import RecipeListComponent from '../components/recipe-list-component'
+import RecipeDetailComponent from '../components/recipe-detail-component'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    RecipeDetailComponent,
+    RecipeListComponent,
+    AddRecipeComponent
   }
 }
 </script>
+<style>
+  a {
+    text-decoration: none;
+    color: darkblue;
+    transition: .3s all ease;
+  }
+
+  .center {
+    text-align: center;
+  }
+
+  a:hover {
+    cursor: pointer;
+    opacity: .7;
+    text-decoration: underline;
+  }
+
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+    height: 100vh;
+  }
+
+  .columns {
+    display: flex;
+  }
+
+  .detail, .list {
+    width: 50%;
+    border: 1px solid #eee;
+  }
+
+  .list {
+    border-right: 0;
+  }
+
+  .btn {
+    border-radius: 5px;
+    background: darkblue;
+    color: #fff;
+    padding: 6px 14px;
+    cursor: pointer;
+  }
+
+  .btn:disabled {
+    background-color: #eee;
+    color: black;
+    cursor: not-allowed;
+  }
+
+  .btn.remove {
+    background: darkred;
+  }
+
+  .btn.secondary {
+    background: grey;
+  }
+</style>
